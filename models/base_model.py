97eba07c6a43e6dbbd8e7109f893967cef6052a8
NiniolaX <loveafinni16@gmail.com>
2023-12-29 01:54:05 +0100

    Made corrections to task 1: (i) Modified class to only update updated_at attribute after the save() method is called; (ii) Added more test cases to test_base_model.py file

--- models/base_model.py
+++ models/base_model.py
@@ -35,34 +35,32 @@ class BaseModel:
     """
 
     def __init__(self, **kwargs):
-        """Initializes a new instance of the class"""
+        """Constructs a new instance of the class"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
-    def __setattr__(self, name, value):
-        """
-        Modify setatrr method to update update_at whenever a change is
-        made to an instance
-        """
-        super().__setattr__(name, value)
-        if name != 'updated_at':
-            self.updated_at = datetime.now()
-
     def __str__(self):
-        """Returns the string representation of an instance of the class"""
+        """
+        Returns string representation of instance in format:
+        "[<class name>] (<self.id>) <self.__dict__>"
+        """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """Updates attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
-        Returns a dictionary containing all keys/values of a __dict__ of
-        the instance
+        Returns a dictionary containing all keys/values of __dict__ of instance
         """
-        obj_dict = self.__dict__
+        obj_dict = {}
         obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = obj_dict['created_at'].isoformat()
-        obj_dict['updated_at'] = obj_dict['updated_at'].isoformat()
+        for key, value in self.__dict__.items():
+            # Convert datetime objects to ISO format
+            if key in ['created_at', 'updated_at']:
+                obj_dict[key] = value.isoformat()
+            else:
+                obj_dict[key] = value
         return obj_dict
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,16 +2,17 @@
 """Unit test for BaseModel"""
 import unittest
 from models.base_model import BaseModel
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class TestBaseModel(unittest.TestCase):
     """Tests the BaseModel class"""
 
     def setUp(self):
+        """Set up BaseModel object for testing"""
         self.model1 = BaseModel()
         self.model2 = BaseModel()
 
-    def test_initiator(self):
+    def test_constructor(self):
         """Tests the class constructor"""
         self.assertIsInstance(self.model1, BaseModel)
         self.assertIsInstance(self.model2, BaseModel)
@@ -23,9 +24,43 @@ class TestBaseModel(unittest.TestCase):
         self.assertNotEqual(self.model1.id, self.model2.id)
 
     def test_attr_created_at(self):
-        """Tests the created_att attribute of instance"""
+        """Tests the created_at attribute of instance"""
         self.assertTrue(hasattr(self.model1, 'created_at'))
         self.assertIsInstance(self.model1.created_at, datetime)
+
+    def test_attr_updated_at(self):
+        """Tests the updated_at attribute of instance"""
+        self.assertTrue(hasattr(self.model1, 'updated_at'))
+        self.assertIsInstance(self.model1.updated_at, datetime)
+
+    def test_method_str(self):
+        """Tests the str method"""
+        expected_string = f"[BaseModel] ({self.model1.id}) {self.model1.__dict__}"
+        actual_string = str(self.model1)
+        self.assertMultiLineEqual(expected_string, actual_string)
+
+    def test_method_save(self):
+        """Tests the save public instance method"""
+
+        # Check that created_at == updated_at when instance is created
+        self.assertEqual(self.model1.created_at, self.model1.updated_at)
+
+        # Check that created_at != updated_at after instance is updated
+        self.model1.name = "Model 1"
+        self.model1.save()
+        self.assertNotEqual(self.model1.created_at, self.model1.updated_at)
+
+        # Check that updated_at > created_at after instance is updated
+        self.assertGreaterEqual(self.model1.updated_at, self.model1.created_at)
+
+    def test_method_to_dict(self):
+        """Tests the to_dict public instance method"""
+
+
+    def teardown(self):
+        """Dispose test object"""
+        self.model1.dispose()
+        self.model2.dispose()
 
 if __name__ == "__main__":
     unittest.main()
