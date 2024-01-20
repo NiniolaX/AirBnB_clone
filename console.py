0d3c9067e9b71e5da14b7853eb4e2998b03cc6cd
NiniolaX <loveafinni16@gmail.com>
2024-01-20 17:02:39 +0100

    Added destroy and all commands to console

--- console.py
+++ console.py
@@ -24,7 +24,7 @@ class_list = ["BaseModel"]
 
 class HBNBCommand(cmd.Cmd):
     """This class defines the commands available on our AirBnB Console."""
-    prompt = '(hbnb) '
+    prompt = "(hbnb) "
 
     def do_create(self, args):
         """Creates a new instance of the BaseModel class
@@ -46,9 +46,10 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
 
     def help_create(self):
-        """Help documentation for create command"""
+        """Help documentation for create command."""
         help_text = """
         Create command creates a new instance of a specified class.
+        Prints the id of the new instance on success.
 
         Usage: create <class_name>
         Returns: Nothing
@@ -58,39 +59,40 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, args):
         """
-        Prints the str representation of an instance based on class name and
-        instance id
+        Prints the str representation of an instance based on its class name
+        and instance id.
 
         Args:
-            args(str): class name and instance id
+            args(str): instance class name and id
 
         Returns:
             Nothing
         """
         if args:
-            cmd_args = args.split()  # split aegs to extract class name and id
-
+            cmd_args = args.split()  # split args to extract class name and id
             if len(cmd_args) < 2:
                 print("** instance id missing **")
-
             else:
                 instance_class_name = cmd_args[0]
                 instance_id = cmd_args[1]
                 if instance_class_name not in class_list:
                     print("** class doesn't exist **")
                 else:
+                    # Extract instance from objects in storage
                     all_objs = storage.all()
-                    for obj_key in all_objs.keys():
-                        if obj_key == f"{instance_class_name}.{instance_id}":
-                            obj = all_objs[obj_key]
-                            print(obj)
-                        else:
-                            print("** no instance found **")
+                    instance_key = f"{instance_class_name}.{instance_id}"
+                    if instance_key not in all_objs.keys():
+                        print("** no instance found **")
+                    else:
+                        for obj_key in all_objs.keys():
+                            if obj_key == instance_key:
+                                instance = all_objs[obj_key]
+                                print(instance)  # Print instance
         else:
             print("** class name missing **")
 
     def help_show(self):
-        """Help documentation for show command"""
+        """Help documentation for show command."""
         help_text = """
         Show command prints the string representation of a specified instance
         based on its class name and id.
@@ -101,11 +103,94 @@ class HBNBCommand(cmd.Cmd):
         formatted_help_text = textwrap.dedent(help_text)
         print(formatted_help_text)
 
-    def do_quit(self, line):
+    def do_destroy(self, args):
+        """Deletes an instance based on its class name and id.
+
+        Args:
+            args(str): instance class name and id
+
+        Returns:
+            Nothing
+        """
+        if args:
+            cmd_args = args.split()  # split args to extract class name and id
+            if len(cmd_args) < 2:
+                print("** instance id missing **")
+            else:
+                instance_class_name = cmd_args[0]
+                instance_id = cmd_args[1]
+
+                if instance_class_name in class_list:
+                    all_objs = storage.all()
+                    instance_key = f"{instance_class_name}.{instance_id}"
+                    if instance_key in all_objs.keys():
+                        del all_objs[instance_key]
+                        storage.save()
+                    else:
+                        print("** no instance found **")
+                else:
+                    print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
+
+    def help_destroy(self):
+        """Help documentation for destroy command."""
+        help_text = """
+        Destroy command deletes an instance based on its class name and id.
+
+        Usage: destroy <class_name> <instance_id>
+        Returns: Nothing
+        """
+        formatted_help_text = textwrap.dedent(help_text)
+        print(formatted_help_text)
+
+    def do_all(self, args=None):
+        """
+        Prints the string representation of all instances based or not on the
+        class name.
+
+        Args:
+            args(str): class name (optional)
+
+        Returns:
+            Nothing
+        """
+        all_instances = []
+        all_objs = storage.all()
+        if args:
+            all_args = args.split()
+            class_name = all_args[0]
+            if class_name in class_list:
+                # Print all instances of class_name in storage
+                for obj_key, obj_value in all_objs.items():
+                    if obj_key.startswith(class_name + '.'):
+                        all_instances.append(str(obj_value))
+                print(all_instances)
+            else:
+                print("** class doesn't exist **")
+        else:
+            # Print all instances in storage
+            for obj_key, obj_value in all_objs.items():
+                all_instances.append(str(obj_value))
+            print(all_instances)
+
+    def help_all(self):
+        """Help documentation for all method."""
+        help_text = """
+        All command prints all string representation of all instances based or
+        not on the class name
+
+        Usage: all <class_name> or all
+        Returns: Nothing
+        """
+        formatted_help_text = textwrap.dedent(help_text)
+        print(formatted_help_text)
+
+    def do_quit(self, args):
         """Exits the console.
 
         Args:
-            line(str): command argument(s)
+            args(str): command argument(s)
 
         Returns:
             Bool: True to exit console
@@ -116,16 +201,7 @@ class HBNBCommand(cmd.Cmd):
         """Help documentation for quit command."""
         print("Quit command to exit the program.\n")
 
-    def do_EOF(self, line):
-        """Exits the console.
-
-        Args:
-            line(str): command argument(s)
-
-        Returns:
-            Bool: True to exit console
-        """
-        return True
+    do_EOF = do_quit
 
     def help_EOF(self):
         """Help documentation for EOF command."""
