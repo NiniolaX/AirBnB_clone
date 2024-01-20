5ea188f7b8a28514a85dd080ee6a6682558cb701
NiniolaX <loveafinni16@gmail.com>
2024-01-20 03:43:20 +0100

    Wrote help documentation for all commands and wrote documentation for __init__ module

--- console.py
+++ console.py
@@ -22,13 +22,23 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
     def do_quit(self, line):
-        """Exits the interpreter."""
+        """Quit command to exit the program."""
         return True
+
+    def help_quit(self):
+        """Help documentation for quit command."""
+        print("Quit command to exit the program")
+
+    def do_EOF(self, line):
+        """EOF command to exit the program."""
+        return True
+
+    def help_EOF(self):
+        """Help documentation for EOF command."""
+        print("EOF command to exit the program.")
 
     def emptyline(self):
         pass
-
-    do_EOF = do_quit
 
 
 if __name__ == "__main__":
--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""This module creates a unique FileStorage instance for the program"""
+
+
 from models.engine import file_storage
 
 storage = file_storage.FileStorage()
