3915908730b823d5f1fd8e7b181114a5c3249765
NiniolaX <loveafinni16@gmail.com>
2024-01-20 03:57:06 +0100

    Added more detailed documentation for HBNBCommand class

--- console.py
+++ console.py
@@ -22,24 +22,38 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
     def do_quit(self, line):
-        """Quit command to exit the program."""
+        """Exits the console.
+
+        Args:
+            line(str): command argument(s)
+
+        Returns:
+            Bool: True to exit console
+        """
         return True
 
     def help_quit(self):
         """Help documentation for quit command."""
-        print("Quit command to exit the program")
+        print("Quit command to exit the program.\n")
 
     def do_EOF(self, line):
-        """EOF command to exit the program."""
+        """Exits the console.
+
+        Args:
+            line(str): command argument(s)
+
+        Returns:
+            Bool: True to exit console
+        """
         return True
 
     def help_EOF(self):
         """Help documentation for EOF command."""
-        print("EOF command to exit the program.")
+        print("EOF command to exit the program.\n")
 
     def emptyline(self):
         pass
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     HBNBCommand().cmdloop()
